50df6552d81821ffc2eecd6c9daf1e51794b7e4d
Sergei Drachev <swkutulu@gmail.com>
2025-12-13 14:04:27 +0700

    add docker

--- backend/src/abcdef/management/commands/create_admin.py
+++ backend/src/abcdef/management/commands/create_admin.py
@@ -6,11 +6,12 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def put_superuser(self):
         User = get_user_model()
-        obj_user, _ = User.objects.get_or_create(username='atrucks')
-        obj_user.set_password('atrucks')
-        obj_user.is_superuser = True
-        obj_user.is_staff = True
-        obj_user.save()
+        obj_user, created = User.objects.get_or_create(username='atrucks')
+        if created:
+            obj_user.set_password('atrucks')
+            obj_user.is_superuser = True
+            obj_user.is_staff = True
+            obj_user.save()
 
     @transaction.atomic
     def handle(self, *args, **options):
